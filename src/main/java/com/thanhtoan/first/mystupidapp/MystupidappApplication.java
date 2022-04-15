a1d2a8facb01a6d7bf10b6f8fc212aae9f8a7fbd
Desmond_SF <thanhtoan.nguyen95@gmail.com>
2022-04-16 00:28:43 +0700

    stupid JAV Spring boot

---
+++ src/main/java/com/thanhtoan/first/mystupidapp/MystupidappApplication.java
@@ -0,0 +1,13 @@
+package com.thanhtoan.first.mystupidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystupidappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystupidappApplication.class, args);
+	}
+
+}
